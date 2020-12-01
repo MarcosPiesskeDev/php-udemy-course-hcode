e4fef632e2fe3a19bc9e7a94d20a629bbd814d3a
Marcos Piesske <marcos.piesske@endurance.com>
2020-11-30 22:40:48 -0300

    Creating class about Interfaces

---
+++ objectOriented/class_7/index.php
@@ -0,0 +1,15 @@
+<html>
+    <a href="../">Back</a>
+    <br>
+    <br>
+    <br>
+</html>
+
+<?php
+
+require_once 'Vehicle.php';
+require_once 'Car.php';
+
+$civic = new Car();
+
+$civic->gearshift(1);
